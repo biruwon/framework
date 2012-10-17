76444f78a8e4ea93177cfce0c1b0f5a4ead44885
Antonio Jesús <ajesus@neventum.com>
2012-10-17 18:31:26 +0200

    Added httpkernel component

--- src/app.php
+++ src/app.php
@@ -3,6 +3,17 @@
 use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Response;
 
+class LeapYearController
+{
+  public function indexAction($year)
+  {
+    if (is_leap_year($year)) {
+        return new Response('Yep, this is a leap year!');
+    }
+    return new Response('Nope, this is not a leap year.');
+  }
+}
+
 function is_leap_year($year = null) {
 	if (null === $year) {
 		$year = date('Y');
@@ -14,13 +25,7 @@ function is_leap_year($year = null) {
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
 	'year' => null,
-	'_controller' => function ($request) {
-		if (is_leap_year($request->attributes->get('year'))) {
-			return new Response('Yep, this is a leap year!');
-		}
-
-		return new Response('Nope, this is not a leap year!');
-	}
+	'_controller' => array(new LeapYearController(), 'indexAction'),
 )));
 
 return $routes;
